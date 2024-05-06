967b2cac3f85f372b3982f2b2607e0dc9e8444a3
dtfeygush <gs1sloghan1@hotmail.com>
2024-05-06 16:16:27 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function rotateArray(arr, n) {
-  return arr.slice(n).concat(arr.slice(0, n));
+function binarySearch(arr, target) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) low = mid + 1;
+    else high = mid - 1;
+  }
+  return -1;
 }
