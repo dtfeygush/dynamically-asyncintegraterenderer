0626ab2fc9b415554e1a94646d35488b08a07c5f
dtfeygush <gs1sloghan1@hotmail.com>
2024-05-08 08:16:20 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const selectionSortRecursive = (arr, start = 0) => {
-  if (start >= arr.length - 1) {
-    return arr;
+const factorialRecursive = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
   }
-  let minIndex = start;
-  for (let i = start + 1; i < arr.length; i++) {
-    if (arr[i] < arr[minIndex]) {
-      minIndex = i;
-    }
-  }
-  if (minIndex !== start) {
-    [arr[start], arr[minIndex]] = [arr[minIndex], arr[start]];
-  }
-  return selectionSortRecursive(arr, start + 1);
+  return n * factorialRecursive(n - 1);
 };
