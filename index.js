4fd62bcd2c8867c8c3074f3cfceb93320f8f4670
dtfeygush <gs1sloghan1@hotmail.com>
2024-05-09 08:09:44 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function isIsomorphic(s, t) {
+  if (s.length !== t.length) return false;
+  const sMap = new Map();
+  const tMap = new Map();
+  for (let i = 0; i < s.length; i++) {
+    const sChar = s[i];
+    const tChar = t[i];
+    if (
+      (sMap.has(sChar) && sMap.get(sChar) !== tChar) ||
+      (tMap.has(tChar) && tMap.get(tChar) !== sChar)
+    )
+      return false;
+    sMap.set(sChar, tChar);
+    tMap.set(tChar, sChar);
   }
-  return n * factorialRecursive(n - 1);
-};
+  return true;
+}
